e5e82d2d6dd1885bff51f27f411d709ee347bba7
Frankshi <frankshi@easyops.cn>
2020-11-21 23:30:28 +0800

    修改路径

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -7,7 +7,7 @@ const clientConfig = {
   entry:"./src/client/index.js",
   output:{
     filename:"index.js",
-    path:path.resolve(__dirname,"public")
+    path:path.resolve(__dirname,"../public")
   },
 }
 
